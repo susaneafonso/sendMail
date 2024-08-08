36f3b5938d8d13ee79904cb7d2c24245e474d9aa
Suh <133658303+susaneafonso@users.noreply.github.com>
2024-08-08 18:44:04 +0000

    02

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./rout");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
+app.use(cors);
 app.use(routes);
 
 app.listen(PORT, () => {
